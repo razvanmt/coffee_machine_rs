2145e0abaf611a3562b2444b66973a4d513238c3
Razvan Turc <twostepsout@gmail.com>
2023-04-21 21:17:00 +0300

    few comments so I won't forget what I did here

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::io;
 use coffee_machine_rs::{Coffee, Coin};
 
 fn main() {
+    // Create a mutable String buffer on memory for the user input
     let mut user_input: String = String::new();
     let stdin: io::Stdin = io::stdin();
 
+    // Read the stdin and insert it into the mut String user_input
     stdin.read_line(&mut user_input)
     .expect("Invalid input");
 
+    // Using the Coffee new_coffee constructor to initialize choice variable as a variant of the struct according to the user_input
     let choice = Coffee::new_coffee(Coffee::from_str(user_input.trim()).unwrap());
 
     println!("{:?}", choice);
